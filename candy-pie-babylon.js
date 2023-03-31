78f01b02feab3362a9c163577d50d5a6ddf12a13
thierry vergult <thierry@opayra.com>
2023-03-31 15:14:55 +0200

    test copy action

--- candy-pie-babylon.js
+++ candy-pie-babylon.js
@@ -1,4 +1,4 @@
-//#region hi
+//#region hiCandy
 /*
     Candy pie
     - short: a 3d pie chart, on top of babylon.js
@@ -41,7 +41,7 @@
       ]
     
 */
-//#endregion hi
+//#endregion hiCandy
 
 candyPie = {};
 
